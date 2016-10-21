b49d90190402c850d25d58eedcbb092158d3794d
Heman Gandhi <hemangandhi@gmail.com>
2016-10-21 01:23:09 -0400

    more meaningful text in matrix effect

--- js/matrix_text.js
+++ js/matrix_text.js
@@ -7,9 +7,25 @@ c.height = window.innerHeight;
 c.width = window.innerWidth;
 
 //katakana characters - taken from the unicode charset
-var chinese = []
-for(var i = 0; i < 96; i++){
-    chinese.push(String.fromCodePoint(0x30a0 + i));
+var meaningfuls = ["\u79c1\u306f\u30d8\u30de\u30f3\u30ac\u30f3\u30b8 ", //watashi wa heman ganji
+                   "\u65e5\u672c\u8a9e\u308f\u304b\u308a\u307e\u3059 ", //nihongo wakarimasu
+                   "\u30af\u30ed\u30b8\u30e3\u30d7\u30ed\u30b0\u30e9\u30df\u30cc\u30b0\u8a00\u8a9e\u306f\u5927\u597d ",
+                   //kurojya puroguraminugu gengo wa daisuki
+                   "Bienvenido \u00e1 mi curriculum interactivo ", //accented a...
+                   "Aprend\u00ed espa\u00f1ol en la preparatoria ", //accented i and n-yeh.
+                   "\u306f\u3058\u3081\u3066\u3001\u3088\u308d\u3057\u304f\u304a\u306d\u304c\u3044\u3057\u307e\u3059 ",
+                   //hajimete, yoroshiku onegaishimasu
+                   "\u30e9\u30c8\u30ac\u30b9\u306b\u30bd\u30d5\u30e2\u3067\u3059 ", //ratogasu ni sofumo desu
+                   "Gracias para leer eso. ",
+                   "\u4f55\u3067\u3053\u308c\u3092\u8aad\u307f\u307e\u3059\u304b? ", //nande kore wo yomimasuka?
+                   ":(){ :|: & };: ", //fork bomb
+                   "\u82f1\u8a9e\u3082\u308f\u304b\u308a\u307e\u3059 ", //eigo mo wakarimasu
+                   "La vida esta loca ",
+                   "\u56f2\u7881\u3082\u5927\u597d ", //igo mo daisuki
+];
+
+var randInd = function(arr){
+    return Math.floor(Math.random() * arr.length);
 }
 
 var font_size = 10;
@@ -18,8 +34,10 @@ var columns = c.width/font_size; //number of columns for the rain
 var drops = [];
 //x below is the x coordinate
 //1 = y co-ordinate of the drop(same for every drop initially)
-for(var x = 0; x < columns; x++)
-	drops[x] = 1; 
+for(var x = 0; x < columns; x++){
+    var mInd = randInd(meaningfuls);
+	drops[x] = [1, mInd, randInd(meaningfuls[mInd])];
+}
 
 //drawing the characters
 function draw(){
@@ -34,17 +52,20 @@ function draw(){
 	for(var i = 0; i < drops.length; i++)
 	{
 		//a random chinese character to print
-		var text = chinese[Math.floor(Math.random()*chinese.length)];
+        var charInd = (drops[i][0] + drops[i][2]) % meaningfuls[drops[i][1]].length;
+		var text = meaningfuls[drops[i][1]][charInd];
 		//x = i*font_size, y = value of drops[i]*font_size
-		ctx.fillText(text, i*font_size, drops[i]*font_size);
+		ctx.fillText(text, i*font_size, drops[i][0]*font_size);
 		
 		//sending the drop back to the top randomly after it has crossed the screen
 		//adding a randomness to the reset to make the drops scattered on the Y axis
-		if(drops[i]*font_size > c.height && Math.random() > 0.975)
-			drops[i] = 0;
+		if(drops[i][0]*font_size > c.height && Math.random() > 0.9){
+            var mInd = randInd(meaningfuls);
+			drops[i] = [0, mInd, randInd(meaningfuls[mInd])];
+        }
 		
 		//incrementing Y coordinate
-		drops[i]++;
+		drops[i][0]++;
 	}
 }
 
