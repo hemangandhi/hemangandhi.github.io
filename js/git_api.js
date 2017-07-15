4aaa147b4e98a16580920e623d767470fb271289
Heman Gandhi <hemangandhi@gmail.com>
2017-07-15 18:49:32 -0400

    my stupidity

--- js/git_api.js
+++ js/git_api.js
@@ -1,10 +1,37 @@
-// https://api.github.com/users/hemangandhi/events?page=2
+//promiseFor :: (Monad m) => a -> (a -> Bool) -> (a -> b -> a) -> (a -> m b) -> m a
+//The then arg is return::(m a)
+function promiseFor(state, cond, retState, mkPromise, then){
+    if(!cond(state)) then(state);
 
-function getGitEvents(then){
-    function getInner(pg){
-        
-    }
-
-    var pg = 1;
-    $.get()
+    mkPromise(state).done(function(d){
+        promiseFor(cond, mkPromise, retState(state, d));
+    });
 }
+
+function getPagesTillNone(baseURL, onPage){
+    promiseFor({page: 1, cond: true},
+        function(s){return s.cond;},
+        function(s, d){
+            if(d.length > 0)
+                onPage(s.page, d);
+            return {page: s.page + 1, cond: d.length > 0};
+        },
+        function(s){
+            return $.get(baseURL + s.page);
+    });
+}
+
+function drawRepo(repo){
+    $('#' + repo.id).html(repo.name +'(' + repo.description + ') has ' + repo.counter + ' events!');
+}
+
+function renderPage(){
+    getPagesTillNone('https://api.github.com/users/hemangandhi/events?page=',
+        function(_, data){
+            promiseFor({idx: 0, repos: {}},
+                function(s){return s.idx < data.length;},
+                function(s, d){}
+        });
+}
+
+$(document).ready(renderPage);
